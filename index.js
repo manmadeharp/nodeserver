db8c68af2cae8cfa6731f49f45cf0761bf0dc536
Finbar <finbarccowan@gmail.com>
2020-10-09 11:41:34 +0100

    2nd commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const hbs = require('express-handlebars')
 const path = require('path')
 const app = express();
-const GetWeather = require('./lib/getWeather');
+const getWeather = require('./lib/getWeather');
+
 require('dotenv').config();
 app.engine('.hbs', hbs({
     extname: '.hbs',
@@ -12,12 +13,13 @@ app.use('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', async (req, res) => {
-    let data = await GetWeather();
+    let data = await getWeather();
     let desc = data.weather[0].description;
     let temp = data.main.temp;
-    
+   res.render('index', {getWeather: 'weather', desc, temp}) 
 });
 
 app.listen(3001, () =>{
     console.log('Server listening on port 3000');
+    console.log(__dirname)
 });
